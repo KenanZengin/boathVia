e0b3be45605484e03b044404c8612b797796acee
fener bahce <xrpchzbtt@gmail.com>
2024-04-11 12:58:00 +0300

    prisma update

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global{
-    var prisma: PrismaClient | undefined;
-}
+// declare global{
+//     var prisma: PrismaClient | undefined;
+// }
 
-export const db = globalThis.prisma || new PrismaClient();
+// export const db = globalThis.prisma || new PrismaClient();
 
-if(process.env.NODE_ENV !=="production") globalThis.prisma = db;
+// if(process.env.NODE_ENV !=="production") globalThis.prisma = db;
 
-//TODO : When producation then db = new PrismaClient();
+export const db = new PrismaClient();
